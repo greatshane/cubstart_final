6243c98a3f33789d50fb3732758e0b9896371849
David Shane <davidshane@berkeley.edu>
2022-04-30 15:51:54 -0700

    can't get verify to work

--- scripts.js
+++ scripts.js
@@ -1,105 +1,174 @@
+// const hash = window.location.hash
+// .substring(1)
+// .split('&')
+// .reduce(function (initial, item) {
+//   if (item) {
+//     var parts = item.split('=');
+//     initial[parts[0]] = decodeURIComponent(parts[1]);
+//   }
+//   return initial;
+// }, {});
+// window.location.hash = '';
+
+// // Set token
+// let _token = hash.access_token;
+
+// const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// // Replace with your app's client ID, redirect URI and desired scopes
+// const clientId = '298a79f00919479da3ace75f2cc3b16d';
+// const clientSecret = "1379e727094c4f8f99e2a6178b8ac4a8";
+// var encodedData = window.btoa(clientId + ':' + clientSecret);
+// const redirectUri = 'https://spotify-recommendations.glitch.me/';
+// const scopes = [
+//   'streaming',
+//   'user-read-birthdate',
+//   'user-read-private',
+//   'user-modify-playback-state',
+//   'user-top-read'
+// ];
+
+// // If there is no token, redirect to Spotify authorization
+// if (!_token) {
+//   window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token`;
+// }
+
 const runCoco = async () => {
     // BEGIN PART 3
-    const net = await cocoSsd.load();
+    // const net = await cocoSsd.load();
     console.log("loaded correctly");
-    detect(net);
+    // detect(net);
     // END PART 3
+    getCaption()
 };
 
-const detect = async (net) => {
-    const img = document.getElementById("img");
-    const imgWidth = img.width;
-    const imgHeight = img.height;
+// const detect = async (net) => {
+//     // const img = document.getElementById("img");
+//     // const imgWidth = img.width;
+//     // const imgHeight = img.height;
 
-    // Set canvas height and width
-    const canvas = document.getElementById("mesh");
-    // BEGIN PART 4
+//     // Set canvas height and width
+//     const canvas = document.getElementById("mesh");
+//     // BEGIN PART 4
     
-    canvas.width = imgWidth;
-    canvas.height = imgHeight;
+//     canvas.width = imgWidth;
+//     canvas.height = imgHeight;
     
-    // END PART 4
+//     // END PART 4
 
-    // Make predictions
-    // BEGIN PART 5
-    const obj = await net.detect(img);
-    // END PART 5
+//     // Make predictions
+//     // BEGIN PART 5
+//     //const obj = await net.detect(img);
+//     // END PART 5
 
-    // Draw mesh
-    // BEGIN PART 6
-    const ctx = canvas.getContext("2d");
-    // END PART 6
-    drawRect(obj, ctx);
-    // Generate caption
-    getCaption(obj);
-    // Make image visible after applying boxes
-    img.style.visibility = "visible";
-};
+//     // Draw mesh
+//     // BEGIN PART 6
+//     const ctx = canvas.getContext("2d");
+//     // END PART 6
+//     //drawRect(obj, ctx);
+//     // Generate caption
+//     getCaption();
+//     // Make image visible after applying boxes
+//     img.style.visibility = "visible";
+// };
 
-const drawRect = (predictions, ctx) => {
-    // Loop through each prediction
-    predictions.forEach((prediction) => {
-        // Extract boxes and classes
-        const [x, y, width, height] = prediction["bbox"];
-        const text = prediction["class"];
+// const drawRect = (predictions, ctx) => {
+//     // Loop through each prediction
+//     predictions.forEach((prediction) => {
+//         // Extract boxes and classes
+//         const [x, y, width, height] = prediction["bbox"];
+//         const text = prediction["class"];
 
-        // Set styling
-        const color = Math.floor(Math.random() * 16777215).toString(16);
-        ctx.strokeStyle = "#" + color;
-        ctx.font = "18px Arial";
+//         // Set styling
+//         const color = Math.floor(Math.random() * 16777215).toString(16);
+//         ctx.strokeStyle = "#" + color;
+//         ctx.font = "18px Arial";
 
-        // Draw rectangles and text
-        ctx.beginPath();
-        ctx.fillStyle = "#" + color;
-        ctx.fillText(text, x, y);
-        ctx.rect(x, y, width, height);
-        ctx.stroke();
-    });
-};
-const getCaption = (predictions) => {
-    predictions.forEach(async (prediction) => {
-        const caption = document.getElementById("caption");
-        const imger = document.getElementById("imgholder");
-        const entity = prediction["class"];
-        try {
-            // Access token provided
-            const accessToken = "574a554e5a8cad9b2666048e80c20365f33323fc";
-            // Store the response from GET request
-            const response = await axios.get(
-            `https://owlbot.info/api/v4/dictionary/${entity}`,
-            { headers: { Authorization: `Token ${accessToken}` } }
-            );
-            // Retrieve the data portion of the response
-            const data = response.data;
-            // From the data, get the first entry of the definitions
-            const entry = data.definitions[0];
-            let lineText;
-            let imageText;
-            if (entry.example) {
-                lineText = entry.example;
-            } else {
-                lineText = entry.definition;
-            }
-            if (entry.emoji) {
-                lineText += " " + entry.emoji;
-            }
-            lineText += " #" + entity;
+//         // Draw rectangles and text
+//         ctx.beginPath();
+//         ctx.fillStyle = "#" + color;
+//         ctx.fillText(text, x, y);
+//         ctx.rect(x, y, width, height);
+//         ctx.stroke();
+//     });
+// };
+const getCaption = async () => {
 
-            imageText = entry.image_url;
-            const line = document.createElement("p");
-            line.innerText = lineText;
-            
-            caption.appendChild(line);
-
-            const imag = document.createElement("img");
-            imag.src = imageText; 
-            imag.height = 200;
-            imag.width = 400;
-            caption.appendChild(imag);
-        } catch (error) {
-            console.log(error);
+    const clientId = '298a79f00919479da3ace75f2cc3b16d';
+    const clientSecret = '1379e727094c4f8f99e2a6178b8ac4a8';
+    var encodedData = window.btoa(clientId + ':' + clientSecret);
+    // predictions.forEach(async (prediction) => {
+    const caption = document.getElementById("caption");
+    // const imger = document.getElementById("imgholder");
+    // const entity = prediction["class"];
+    try {
+        // Access token provided
+        //const accessToken = "BQCifKY3heTA1SJhD2rrlS4MJGM_0PYYWfSb_hRSAnMl5rijHayzFfYP3IysiY-bwD9DjcaZ0D8Md0tFgOjK8a7APPAi2NXDrByTLglwib-xVWVwSUED-LitiRbOQ_fkfZnNE0KkDkti54q5-J5gwewxYsEm5is";
+        // // Store the response from GET request
+        var authorizationHeaderString = 'Authorization: Basic ' + encodedData;
+        const response = await axios.post(
+            'https://accounts.spotify.com/api/token',
+        { headers: {authorizationHeaderString } , 
+        form: {
+            grant_type: 'client_credentials'
+          },
+          json: true
         }
-    })
+
+        );
+
+      
+
+        
+
+        //'https://accounts.spotify.com/api/token'
+
+        //https://api.spotify.com/v1/recommendations?" + 'limit=1' + '&min_danceability=1'
+        // $.ajax({
+        //     url: "https://api.spotify.com/v1/recommendations?" + 'limit=1' + '&min_danceability=1' ,
+        //     type: "GET",
+        //     beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken );},
+        //     success: function(data) { 
+        //       console.log(data)
+        //       let trackUri = data.tracks[0].uri
+        //       print(data)
+        //       //play(deviceId, trackUri);
+        //     }
+        //   });
+
+
+        // Retrieve the data portion of the response
+        const data = response;
+        // From the data, get the first entry of the definitions
+        const entry = data;
+        let lineText;
+        //let imageText;
+        // if (entry.example) {
+        //     lineText = entry.example;
+        // } else {
+        //     lineText = entry.definition;
+        // }
+        // if (entry.emoji) {
+        //     lineText += " " + entry.emoji;
+        // }
+        // lineText += " #" + entity;
+
+        // imageText = entry.image_url;
+        lineText = entry.access_token;
+        const line = document.createElement("p");
+        line.innerText = lineText;
+        
+        caption.appendChild(line);
+
+        // const imag = document.createElement("img");
+        // imag.src = imageText; 
+        // imag.height = 200;
+        // imag.width = 400;
+        // caption.appendChild(imag);
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
 
